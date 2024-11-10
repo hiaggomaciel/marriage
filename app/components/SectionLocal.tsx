d4e4258866252e7dc9a707f6fffcc3af559126f5
Hiaggo Maciel <hiaggo_firmiano@hotmail.com>
2024-11-10 11:09:50 -0300

    feat: secao local

---
+++ app/components/SectionLocal.tsx
@@ -0,0 +1,9 @@
+import styles from '../styles/Home.module.css';
+
+export default function SectionLocal() {
+  return (
+    <section id="casal" className={`${styles.section} ${styles.casalSection}`}>
+      
+    </section>
+  )
+}
